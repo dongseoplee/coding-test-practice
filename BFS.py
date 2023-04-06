8aea8db870bc0b8de584385f0d6f79d9167a19e5
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-04-06 20:27:53 +0900

    add bfs source code

--- BFS.py
+++ BFS.py
@@ -89,4 +89,69 @@ def bfs(graph):
 bfs(graph)
 print(graph[n - 1][m - 1])
 
+#7576번 토마토
+# 동시 출발점이 2개인 문제
+# 초기에 1인 row, col 큐에 넣어둬라
+import sys
+from collections import deque
 
+# 1 익은, 0 안익은, -1 없음
+
+m, n = map(int, sys.stdin.readline().split())
+graph = []
+
+for _ in range(n):
+    tempList = list(map(int, sys.stdin.readline().split()))
+    graph.append(tempList)
+
+queue = deque()  # 큐를 bfs 함수 밖에서 선언
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            queue.append((i, j))  # 큐에 바로 삽입
+
+
+# print(graph)
+def bfs(bfs_graph):
+    # for i in range(len(ripeList)):
+    # queue.append(ripeList.pop())
+    # print('queue', queue)
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+
+    while queue:
+        x, y = queue.popleft()
+        # print(x, y)
+        for i in range(4):
+            nx = x + dx[i]  # 좌우 열의 갯수 col
+            ny = y + dy[i]  # 위아래 행의 갯수 row
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if bfs_graph[nx][ny] == 0:
+                # print(nx, ny)
+                queue.append((nx, ny))
+                # dist += 1
+                bfs_graph[nx][ny] = bfs_graph[x][y] + 1
+
+    return bfs_graph
+
+
+# 1의 row, col을 리스트 형태로 bfs 함수로 전달
+# 1이면 bfs 돌려라
+res_graph = bfs(graph)
+
+res = 0
+for line in res_graph:
+    for tomato in line:
+        if tomato == 0:
+            print(-1)
+            exit()  # 프로그램 종료
+
+    res = max(res, max(line))
+
+print(res - 1)
+
+# 큐를 밖에서 선언 후 바로 1인 row, col append 하는 것
+# 결과 그래프를 이중 for문으로 행별로 읽고 행의 값을 판별 하는 것
