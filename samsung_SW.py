03eb007c80ca3c9b5bc0113124964786e17c124a
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-05-22 17:14:30 +0900

    update source code; clear ;; ;; exit ;

--- samsung_SW.py
+++ samsung_SW.py
@@ -16,3 +16,19 @@ for i in range(n):
   sub = 0
 
 print(sum(cntList))
+
+#14501번 퇴사
+import sys
+n = int(sys.stdin.readline())
+dp = [0] * (n + 1)
+schedule = []
+for _ in range(n):
+  schedule.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(n-1, -1, -1): #6~0
+  if i + schedule[i][0] > n:
+    dp[i] = dp[i+1]
+  else:
+    dp[i] = max(schedule[i][1] + dp[i + schedule[i][0]], dp[i+1])
+print(dp[0])
+
