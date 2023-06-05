17c8ad4e4ca225bf2c94519f59bff9c3d8293c78
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-06-05 23:28:56 +0900

    update DP solution

--- DP.py
+++ DP.py
@@ -420,3 +420,17 @@ for i in range(1, n + 1):
 
 # dp[14] = dp[14 - 9] + 1 -> dp[9] + dp[5] -> dp[9] + dp[5 - 4] + 1 -> dp[9] + dp[4] + dp[1]
 print(dp[n])
+
+#11722번 가장 긴 감소하는 부분 수열
+import sys
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+dp = [1] * n
+
+for i in range(1, n):
+  for j in range(i):
+    if a[i] < a[j]:
+      dp[i] = max(dp[i], dp[j] + 1)
+
+# print(dp)
+print(max(dp))
