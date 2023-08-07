50013265d59c18011a99a2a92ff23929633a56dd
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-08-07 21:46:59 +0900

    add solution

--- samsung_SW.py
+++ samsung_SW.py
@@ -32,3 +32,40 @@ for i in range(n-1, -1, -1): #6~0
     dp[i] = max(schedule[i][1] + dp[i + schedule[i][0]], dp[i+1])
 print(dp[0])
 
+
+#14888번 연산자 끼워넣기
+import sys
+from itertools import permutations
+n = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+opNum = list(map(int, sys.stdin.readline().split()))
+opList = ['+', '-', '*', '/']
+op = []
+res = []
+for j in range(4):
+  for k in range(opNum[j]):
+    op.append(opList[j])
+
+# print(op)
+maximum = 1e9
+minimum = -1e9
+def calculator():
+  global maximum, minimum
+  for case in permutations(op, n-1): #permutation 두번째 인자는 크기를 넣어준다.
+    calNum = nums[0]
+    for i in range(1, n): #12345
+      if case[i-1] == '+':
+        calNum += nums[i]
+      if case[i-1] == '-':
+        calNum -= nums[i]
+      if case[i-1] == '*':
+        calNum *= nums[i]
+      if case[i-1] == '/':
+        calNum = int(calNum / nums[i])
+
+    res.append(calNum)
+
+calculator()
+# print(res)
+print(max(res))
+print(min(res))
