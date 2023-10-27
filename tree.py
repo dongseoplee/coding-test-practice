6550d9781c54c056fd91c32a70da6400a63ffdbc
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-10-27 11:16:40 +0900

    add solution

--- tree.py
+++ tree.py
@@ -20,3 +20,55 @@ for i in range(1, k+1): #층 수
 for i in range(k):
     print(*res[i+1])
 # print(res)
+
+#1991번 트리 순회
+# 전위 순회(preorder traverse) : 뿌리(root)를 먼저 방문
+# 중위 순회(inorder traverse) : 왼쪽 하위 트리를 방문 후 뿌리(root)를 방문
+# 후위 순회(postorder traverse) : 하위 트리 모두 방문 후 뿌리(root)를 방문
+# 전위 순회한 결과 : ABDCEFG // (루트) (왼쪽 자식) (오른쪽 자식)
+# 중위 순회한 결과 : DBAECFG // (왼쪽 자식) (루트) (오른쪽 자식)
+# 후위 순회한 결과 : DBEGFCA // (왼쪽 자식) (오른쪽 자식) (루트)
+# 트리를 어떻게 만드는 지가 궁금
+import sys
+n = int(sys.stdin.readline())
+tree = {}
+for _ in range(n):
+    root, left, right = sys.stdin.readline().split()
+    tree[root] = [left, right]
+
+# print(tree)
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+
+# def postorder(root):
+#
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
+# 7
+# A B C
+# B D .
+# C E F
+# E . .
+# F . G
+# D . .
+# G . .
