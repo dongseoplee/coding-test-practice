bb34acc23200f3eebd1bbe582647f5e31b6b5392
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2024-01-05 15:02:18 +0900

    add solution

--- samsung_SW.py
+++ samsung_SW.py
@@ -1351,3 +1351,68 @@ while True:
     if belt.count(0) >= k:
         print(res)
         break
+
+#17822번 원판 돌리기
+from collections import deque
+n, m, t = map(int, input().split())
+data = [deque(int(x) for x in input().split()) for _ in range(n)]
+
+for tc in range(t) :
+    x, d, k = map(int, input().split())
+    # 회전 작업 수행
+    result = 0
+    for i in range(n) :
+        result += sum(data[i])
+        if (i+1) % x == 0 :
+            if d == 0 : # 시계 방향 회전
+                data[i].rotate(k)
+            else : # 반시계 방향 회전
+                data[i].rotate(-k)
+
+    # 원판에 수가 남아 있으면, 인접하면서 수가 같은 것을 제외
+    if result != 0 :
+        remove_value = []
+        for i in range(n) :
+            for j in range(m-1) :
+                if data[i][j] != 0 and data[i][j+1] != 0 and data[i][j] == data[i][j+1] :
+                    remove_value.append((i, j))
+                    remove_value.append((i, j+1))
+            if data[i][0] != 0 and data[i][-1] != 0 and data[i][0] == data[i][-1] :
+                remove_value.append((i, 0))
+                remove_value.append((i, m-1))
+
+        for j in range(m) :
+            for i in range(n-1) :
+                if data[i][j] != 0 and data[i+1][j] != 0 and data[i][j] == data[i+1][j] :
+                    remove_value.append((i, j))
+                    remove_value.append((i+1, j))
+
+        # 원판 갱신
+        remove_value = list(set(remove_value))
+        for i in range(len(remove_value)) :
+            x, y = remove_value[i]
+            data[x][y] = 0
+
+        # 없는 경우 : 원판에 적힌 수의 평균을 구하고, 평균보다 큰 수에서 1을 빼고 작은 수에는 1을 더한다.
+        if len(remove_value) == 0 :
+            sum_value = 0
+            zero_count = 0
+            for i in range(n) :
+                sum_value += sum(data[i])
+                zero_count += data[i].count(0)
+            avg_value = sum_value / (n * m - zero_count)
+
+            for i in range(n) :
+                for j in range(m) :
+                    if data[i][j] != 0 and data[i][j] > avg_value :
+                        data[i][j] -= 1
+                    elif data[i][j] != 0 and data[i][j] < avg_value :
+                        data[i][j] += 1
+    else :
+        break
+
+answer = 0
+for i in range(n) :
+    answer += sum(data[i])
+
+print(answer)
