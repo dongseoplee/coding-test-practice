a8b1b08cfb2a48bdfecd6e1cff0dabd36a83551b
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2024-01-15 16:03:43 +0900

    add solution

--- DFS.py
+++ DFS.py
@@ -625,3 +625,38 @@ for i in range(n):
         res += 1
 
 print(res)
+
+#1937번 욕심쟁이 판다
+import sys
+sys.setrecursionlimit(10**9)
+n = int(sys.stdin.readline())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().split())))
+# print(graph)
+dp = [[0] * n for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y):
+    if dp[x][y]:
+        return dp[x][y]
+    dp[x][y] = 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            continue
+        if graph[x][y] < graph[nx][ny]:
+            dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1)
+    return dp[x][y]
+
+for i in range(n):
+    for j in range(n):
+        if not dp[i][j]:
+            dfs(i, j)
+res = 0
+for m in range(n):
+    res = max(res, max(dp[m]))
+
+print(res)
