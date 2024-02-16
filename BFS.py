fdd2a58c697cc444a1555e7f6f5f7e03bb5d65c5
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2024-02-16 15:17:10 +0900

    add solution

--- BFS.py
+++ BFS.py
@@ -1661,3 +1661,35 @@ for i in range(h):
             answer = max(answer, bfs(i, j))
 
 print(answer)
+
+#미로 탈출
+import sys
+from collections import deque
+# queue = deque()
+n, m = map(int, sys.stdin.readline().split())
+# visited = [[False for _ in range(n)] for _ in range(m)]
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+bfs(0, 0)
+# print(graph)
+print(graph[n-1][m-1])
+# print(visited)
--- DFS.py
+++ DFS.py
@@ -720,3 +720,35 @@ while True:
 print(res)
 
 
+#음료수 얼려 먹기
+import sys
+sys.setrecursionlimit(10**9)
+n, m = map(int, sys.stdin.readline().split())
+graph = []
+visited = [[False for _ in range(m)] for _ in range(n)]
+for _ in range(n):
+    graph.append(list(sys.stdin.readline().rstrip()))
+
+print(graph)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+#방문처리 목적 dfs
+def dfs(x, y):
+    visited[x][y] = True
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx<0 or nx >=n or ny < 0 or ny >=m:
+            continue
+        if graph[nx][ny] == '0' and visited[nx][ny] == False:
+            dfs(nx, ny)
+
+
+res = 0
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == '0' and visited[i][j] == False:
+            dfs(i, j)
+            res += 1
+
+print(res)
