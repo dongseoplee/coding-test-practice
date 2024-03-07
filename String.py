ba0e62243ec2fd6afe4551ec72c8e854363b1711
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2024-03-07 19:43:41 +0900

    add solution

--- String.py
+++ String.py
@@ -149,3 +149,14 @@ for char in str:
     elif 97 <= ord(char) <= 122: #소문자
         ans += chr(ord(char) - 32)
 print(ans)
+
+#2754번 학점계산
+import sys
+set = {'A+': 4.3, 'A0': 4.0, 'A-': 3.7, 'B+': 3.3, 'B0': 3.0, 'B-': 2.7,
+       'C+': 2.3, 'C0': 2.0,'C-': 1.7,
+'D+': 1.3,'D0': 1.0,'D-': 0.7,
+'F': 0.0}
+
+# sys.stdin = open("input.txt", "r")
+score = sys.stdin.readline().rstrip()
+print(set[score])
