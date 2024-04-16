ec8cafc36013305b1dc67c2e9cec1ce747fe70fe
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2024-04-16 12:13:23 +0900

    add solution

--- samsung_SW.py
+++ samsung_SW.py
@@ -2731,3 +2731,88 @@ for k in range(K):
         ci, cj = ci + di[dr], cj + dj[dr]
 
 print(ans)
+
+# 코드트리 포탑 부수기
+# import sys
+# sys.stdin = open("input.txt", "r")
+
+N, M, K = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+turn = [[0]*M for _ in range(N)]
+
+from collections import deque
+def bfs(si, sj, ei, ej):
+    q = deque()
+    v = [[[] for _ in range(M)] for _ in range(N)]
+    q.append((si, sj))
+    v[si][sj] = (si, sj)
+    d = arr[si][sj]
+
+    while q:
+        ci, cj = q.popleft()
+        if (ci, cj) == (ei, ej):
+            arr[ei][ej] = max(0, arr[ei][ej] - d)
+            while True:
+                ci, cj = v[ci][cj]
+                if (ci, cj) == (si, sj):
+                    return True
+                arr[ci][cj] = max(0, arr[ci][cj] - d//2)
+                fset.add((ci, cj))
+        for di, dj in ((0,1), (1,0), (0,-1), (-1,0)):       # 우 하 좌 상
+            ni, nj = (ci + di)%N, (cj + dj)%M
+            if len(v[ni][nj]) == 0 and arr[ni][nj] > 0:
+                q.append((ni, nj))
+                v[ni][nj] = (ci, cj)
+
+    return False
+
+def bomb(si, sj, ei, ej):
+    d = arr[si][sj]
+    arr[ei][ej] = max(0, arr[ei][ej]-d)
+    for di, dj in ((-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)):
+        ni, nj = (ei + di)%N, (ej + dj)%M
+        if (ni, nj) != (si, sj):
+            arr[ni][nj] = max(0, arr[ni][nj] - d//2)
+            fset.add((ni, nj))
+
+for T in range(1, K+1):
+    # [1] 공격자 선정
+    mn, mx_turn, si, sj = 5001, 0, -1, -1
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] <= 0: continue
+            if (mn > arr[i][j]) or (mn == arr[i][j] and mx_turn < turn[i][j]) \
+                or (mn == arr[i][j] and mx_turn == turn[i][j] and i+j > si+sj) \
+                or (mn == arr[i][j] and mx_turn == turn[i][j] and i+j == si+sj and j > sj):
+                    mn, mx_turn, si, sj = arr[i][j], turn[i][j], i, j
+    # [2] 피해자 선정
+    mx, mn_turn, ei, ej = 0, T, N, M
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] <= 0: continue
+            if (mx < arr[i][j]) or (mx == arr[i][j] and mn_turn > turn[i][j]) \
+                    or (mx == arr[i][j] and mn_turn == turn[i][j] and i + j < ei + ej) \
+                    or (mx == arr[i][j] and mn_turn == turn[i][j] and i + j == ei + ej and j < ej):
+                mx, mn_turn, ei, ej = arr[i][j], turn[i][j], i, j
+    # [3] 공격
+    # [3-1] 레이저 공격
+    arr[si][sj] += (N+M)
+    turn[si][sj] = T
+    fset = set()
+    fset.add((si, sj))
+    fset.add((ei, ej))
+    if bfs(si, sj, ei, ej) == False:         # 레이저 경로가 나온다면
+        bomb(si, sj, ei, ej)
+
+    # [3-2] 포탄 공격
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] > 0 and (i, j) not in fset:
+                arr[i][j] += 1
+    cnt = N*M
+    for lst in arr:
+        cnt -= lst.count(0)
+    if cnt <= 1:
+        break
+
+print(max(map(max, arr)))
